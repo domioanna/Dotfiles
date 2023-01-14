4386e7d7aeb64aabb42d007c53564e0bb22596c9
domioanna <dom@domioanna.com>
2023-01-14 01:12:45 +0000

    Changed keymap and added more config settings

--- nvim/lua/user/plugins/nvim-tree.lua
+++ nvim/lua/user/plugins/nvim-tree.lua
@@ -1,3 +1,10 @@
+-- disable netrw at the very start of your init.lua (strongly advised)
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- set termguicolors to enable highlight groups
+vim.opt.termguicolors = true
+
 require('nvim-tree').setup({
     git = {
         ignore = false,
@@ -16,4 +23,4 @@ require('nvim-tree').setup({
 })
 
 vim.api.nvim_set_hl(0, 'NvimTreeIndentMarker', {fg = '#30323E'})
-vim.keymap.set('n', '<Leader>n', ':NvimTreeFindFileToggle<CR>')
+vim.keymap.set('n', '<Leader>1', ':NvimTreeFindFileToggle<CR>')
