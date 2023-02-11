c9dcda404c76d5df8bede03a1b1eae0f5abe3fbd
domioanna <dom@domioanna.com>
2023-02-11 21:51:38 +0000

    Everforest

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -23,10 +23,40 @@ require('packer').init({
 
 local use = require('packer').use
 
+-- use({
+--     'jessarcher/onedark.nvim',
+--     config = function()
+--         vim.cmd('colorscheme onedark')
+
+--         -- Hide the characters in FloatBorder
+--         vim.api.nvim_set_hl(0, 'FloatBorder', {
+--             fg = vim.api.nvim_get_hl_by_name('NormalFloat', true).background,
+--             bg = vim.api.nvim_get_hl_by_name('NormalFloat', true).background,
+--         })
+
+--         -- Make the StatusLineNonText background the same as StatusLine
+--         vim.api.nvim_set_hl(0, 'StatusLineNonText', {
+--           fg = vim.api.nvim_get_hl_by_name('NonText', true).foreground,
+--           bg = vim.api.nvim_get_hl_by_name('StatusLine', true).background,
+--         })
+
+--         -- Hide the characters in CursorLineBg
+--         vim.api.nvim_set_hl(0, 'CursorLineBg', {
+--           fg = vim.api.nvim_get_hl_by_name('CursorLine', true).background,
+--           bg = vim.api.nvim_get_hl_by_name('CursorLine', true).background,
+--         })
+
+--         vim.api.nvim_set_hl(0, 'NvimTreeIndentMarker', { fg = '#30323E' })
+--         vim.api.nvim_set_hl(0, 'IndentBlanklineChar', { fg = '#2F313C' })
+--     end,
+-- })
+
 use({
-    'jessarcher/onedark.nvim',
+    "neanias/everforest-nvim",
+    -- Optional; default configuration will be used if setup isn't called.
     config = function()
-        vim.cmd('colorscheme onedark')
+        require("everforest").setup()
+        vim.cmd('colorscheme everforest')
 
         -- Hide the characters in FloatBorder
         vim.api.nvim_set_hl(0, 'FloatBorder', {
@@ -36,15 +66,15 @@ use({
 
         -- Make the StatusLineNonText background the same as StatusLine
         vim.api.nvim_set_hl(0, 'StatusLineNonText', {
-          fg = vim.api.nvim_get_hl_by_name('NonText', true).foreground,
-          bg = vim.api.nvim_get_hl_by_name('StatusLine', true).background,
-        })
+                fg = vim.api.nvim_get_hl_by_name('NonText', true).foreground,
+                bg = vim.api.nvim_get_hl_by_name('StatusLine', true).background,
+            })
 
         -- Hide the characters in CursorLineBg
         vim.api.nvim_set_hl(0, 'CursorLineBg', {
-          fg = vim.api.nvim_get_hl_by_name('CursorLine', true).background,
-          bg = vim.api.nvim_get_hl_by_name('CursorLine', true).background,
-        })
+                fg = vim.api.nvim_get_hl_by_name('CursorLine', true).background,
+                bg = vim.api.nvim_get_hl_by_name('CursorLine', true).background,
+            })
 
         vim.api.nvim_set_hl(0, 'NvimTreeIndentMarker', { fg = '#30323E' })
         vim.api.nvim_set_hl(0, 'IndentBlanklineChar', { fg = '#2F313C' })
@@ -155,7 +185,8 @@ use({
 use({
     'akinsho/bufferline.nvim',
     requires = 'kyazdani42/nvim-web-devicons',
-    after = 'onedark.nvim',
+    -- after = 'onedark.nvim',
+    after = 'everforest-nvim',
     config = function()
         require('user/plugins/bufferline')
     end
